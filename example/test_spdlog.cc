00c3c6f67e33c36069c1b8f37ea1fd5162f4c7fc
xinhong <xinhong007@126.com>
2022-09-27 17:10:17 +0800

    update:add log name

--- example/test_spdlog.cc
+++ example/test_spdlog.cc
@@ -1,16 +1,20 @@
 #include "log_lib/spdlog_logger.h"
+#include <unistd.h>
 
 int main()
 {
-    LOG_INIT("./logs", "info");
-    LOG(DEBUG, "hello world {}", true);
     LOG_INIT("./logs", "debug");
-    LOG(DEBUG, "hello world {}", 2);
-    SET_LOG_LEVEL(DEBUG);
-    LOG(DEBUG, "Hello world {}", 3);
-    // SET_LOG_SYNC_LEVEL(DEBUG);
-    LOG(DEBUG, "Hello world {}", true);
-    while(true)
-        ;
+    // LOG(DEBUG, "hello world {}", true);
+    // LOG_INIT("./logs", "debug");
+    // LOG(DEBUG, "hello world {}", 2);
+    // SET_LOG_LEVEL(DEBUG);
+    // LOG(DEBUG, "Hello world {}", 3);
+    // // SET_LOG_SYNC_LEVEL(DEBUG);
+    // LOG(DEBUG, "Hello world {}", true);
+    while (true) {
+        LOG(DEBUG, "hello world");
+        usleep(100);
+    }
+        
     return 0;
 }
